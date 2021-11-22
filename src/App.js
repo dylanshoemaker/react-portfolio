bf045e6d8953d8e7ef36a95945f54a5752c686ff
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-21 17:11:19 -0800

    added categories to header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
@@ -9,19 +9,37 @@ import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 function App() {
+  const [categories] = useState([
+    { name: "About" },
+    { name: "Portfolio" },
+    { name: "Contact Me" },
+    { name: "Resume" },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <Router>
-      <Header />
-      <Hero />
-      <Switch>
-        <Route exact path='/react-portfolio' component={About}/>
-        <Route path="/about" component={About} />
-        <Route path="/Portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/resume" component={Resume} />
-      </Switch>
-      <Footer/>
-    </Router>
+    <div>
+      <Router>
+      <Header
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Header>
+      <main>
+        <div>
+          <Hero></Hero>
+          <Switch>
+            <Route exact path="/react-portfolio" component={About} />
+            <Route path="/about" component={About} />
+            <Route path="/Portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/resume" component={Resume} />
+          </Switch>
+          <Footer></Footer>
+        </div>
+      </main>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
+const Header = (props) => {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
-const Header = () => {
+  useEffect(() => {
+    document.title = currentCategory.name;
+  }, [currentCategory]);
 
   return (
-    <header>
-        <h1>
-            Dylan Shoemaker
-        </h1>
-        <nav>
-            <ul>
-                <li>
-                <Link to="/about">About</Link>
-                </li> 
-                <li>
-                <Link to="/portfolio">Portfolio</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact Me</Link>
-                </li>
-                <li>
-                <Link to="/resume">Resume</Link>
-                </li>
-            </ul>
-        </nav>
+    <header className="flex-row px-1">
+      <h2>
+        <a href="/">Dylan Shoemaker</a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li>
+          <Link to="/about"href="#about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact"href="#contact">Contact</Link>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
-
-}
+};
 
 export default Header;
