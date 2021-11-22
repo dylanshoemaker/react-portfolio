b66ec488e8b78fb4a10e10852c5d0868491cec13
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-21 17:22:34 -0800

    add project to portfolio

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,6 +4,7 @@ import coFeFe from "../assets/images/coffee.jpg"
 import jedkh from "../assets/images/jedkh2.PNG"
 import exPress from "../assets/images/express.png"
 import pickleRick from "../assets/images/show-me-what-you-got-walkthrough.gif"
+import birdPerson from "../assets/images/landingpage-screenshot.JPG"
 const projectData = [
   {
     name: "Game Stork Group Project",
@@ -44,6 +45,12 @@ const projectData = [
     gitHubLink: "https://github.com/dylanshoemaker/show-me-what-you-got",
     liveLink: "https://pdx.cdn.nv.instructuremedia.com/originals/o-3BrQbP4oYmPPUGxcNL1SLqYGsF54uy1/transcodings/t-3Tam8sEHFnabgMJLyfJ5om6HGD5dPfd.mp4?&Expires=1637107589&Signature=qbuan8BFH0rbzb-V9nwD7f5m0FhwtAPb4fjMr7QPV97RaUVkCNdq34Uo0PrgvISpDcdRIhUwQ8PrjTBRbQWhOs5WNd7WRlRuGZyrQJXshADUpBwLL8mwqbiozGNwuhDGS4myD35bpAWjdnpSl0VnyIMyJ8YbyYba2GqIARDDs5U2ISBZWGIXo7JbUz4w3j4NrfbHKM5lpXhJ1oKtgGhIZq26lDg8dw75ePVj1~AE4bhJuGwNkaofLf05~R7Qyepv8hA~~9VjxEIFMOausPusgpVzEb-sgtwqSSJyk99VDoItxOJ6JFHqMDiQcZa3ZiPlUCPjJYZL6TGMAAoNM6bRzQ__&Key-Pair-Id=APKAJLP4NHW7VFATZNDQ",
     screenShot: pickleRick,
+  },
+  {
+    name: "Salonme",
+    gitHubLink: "https://github.com/dylanshoemaker/salonme",
+    liveLink: "https://salonme.herokuapp.com/",
+    screenShot: birdPerson,
   },
 ];
 
