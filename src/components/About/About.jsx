fb9d4b96239e248b7b537413064cd6ee4c7b1807
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-27 15:22:38 -0800

    update about:

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,11 +3,24 @@ import React from "react";
 function About() {
   return (
     <section className="my-5 step">
-      <h1 className="full-text" id="about">Who am I?</h1>
+      <h1 className="full-text" id="about">
+        Who am I?
+      </h1>
       <div className="my-2">
-      <p>Hello, thank you for taking the time to look at my portfolio! My name is Dylan Shoemaker and I love solving puzzles. I've always wanted to know how something works, how it can be broken and how it can be fixed. 
-      <br></br>The main thing I love about web design is that you get to make your own puzzle. Some puzzles are easy to solve, some are a bit more difficult, but I’ve never found a puzzle I have not enjoyed solving. I’m an avid fan of technology and software; on my quest to never be bored I'm constantly researching and learning how to make the most of all the tools that we have to make magic happen on our screens.
-      </p>
+        <p>
+          Hello, thank you for taking the time to look at my portfolio! My name
+          is Dylan Shoemaker and I love solving puzzles. I've always wanted to
+          know how something works, how it can be broken down into individual
+          pieces and how it can be put back together.
+        </p>
+        <p>
+          I’m an avid fan of technology and software and my goal in life is to
+          learn as much as I can about the technology that fuels how we interact
+          with the world around us. I've recently obtained a Certificate in Full
+          Stack Web Development from the University of Utah and I'm currently looking
+          for any career opportunities with a company thats goal is to make a difference
+          or to solve a problem.
+        </p>
       </div>
     </section>
   );
