7681570827f473b44cd4544df9e33abd05892085
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-27 15:53:27 -0800

    redo footer with bulma and fontawesome icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,58 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import './Footer.css';
+
 
 function Footer() {
-  return (
-    <section id="contact-me" >
-      <div className="step">
-        <nav>
-          <ul>
-            <li>
-              <a href="mailto:pleasehireme@dylanshoemaker.com">
-                PleaseHireMe@DylanShoemaker.com
-              </a>
-            </li>
-            <li>
-              <a href="tel:7024166673">702-416-6673</a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/dylan-shoemaker/">
-                Linkedin
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/DylanShoemaker">Github</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </section>
-  );
+	return (
+		
+		<div className="footer pt-6 pb-6">
+			<div class="content has-text-centered pt-2">
+			
+							<div className="is-divider"></div>
+							<div className="columns about-links">
+								<div className="column">
+									<p className="heading is-size-5">
+										<strong>Give me a ring</strong>
+									</p>
+									<p className="subheading is-size-5">702-416-6673</p>
+								</div>
+								<div className="column">
+								<p className="heading is-size-5">
+										<strong>Email Me</strong>
+									</p>
+									<p className="subheading is-size-5"><a href='mailto:PleaseHireMe@DylanShoemaker.com'>PleaseHireMe@DylanShoemaker.com</a></p>
+								</div>
+								<div className="column">
+								<p className="heading is-size-5">
+										<strong>Download my resume</strong>
+									</p>
+									<p className="subheading is-size-5">	<Link  target='_blank' to='/resume/dylan-shoemaker-resume.pdf'>Download</Link></p>
+									
+								</div>
+							</div>
+
+							<div className="column">
+								<a href="https://github.com/dylanshoemaker" target='_blank' rel="noreferrer" >
+								<FontAwesomeIcon icon={faGithub } className='font-awesome'></FontAwesomeIcon>
+								</a>
+
+								<a href="https://www.linkedin.com/in/dylan-shoemaker/" target='_blank'  rel="noreferrer" >
+								<FontAwesomeIcon icon={faLinkedin } className='font-awesome'></FontAwesomeIcon>
+								</a>
+
+								<a href="https://www.instagram.com/they_call_me_shoe/" target='_blank' rel="noreferrer" >
+								<FontAwesomeIcon icon={faInstagram} className='font-awesome'></FontAwesomeIcon>				
+								</a>
+
+								<p className='is-size-6 mt-5'>&copy; Copyright 2021 All Content, Dylan Shoemaker | All rights reserved</p>
+							</div>
+				
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
