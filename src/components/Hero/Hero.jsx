2c017f38ef355deba1e479e34152881db62f3693
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-09 11:15:50 -0800

    make buttons more mobile friendly

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,40 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Hero.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Hero.css";
 
 function Hero() {
-	return (
-		<>
-			<section className="hero is-large">
-				<div className="hero-body">
-					<div className="container has-text-centered">
-						<h1 className="subtitle profession">Hello there, I'm</h1>
-						<h2 className="title">Dylan</h2>
-						<h1 className="subtitle profession">
-							A lifelong learner who likes to code
-						</h1>
-						<div className='is-hidden-mobile'>
-						<Link to="/about">
-							<button className="button is-outlined is-medium mx-5" data-aos="fade-up">
-								About
-							</button>
-						</Link>
-						<Link to="/portfolio">
-							<button className="button is-outlined is-medium mx-5" data-aos="fade-down" >
-								Portfolio
-							</button>
-						</Link>
-						<Link to="/resume">
-							<button className="button is-outlined is-medium mx-5" data-aos="fade-up">
-								Resume
-							</button>
-						</Link>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className="hero is-large">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="subtitle profession is-size-4-mobile">Hello there, I'm</h1>
+            <h2 className="title is-size-4-mobile">Dylan</h2>
+            <h1 className="subtitle profession is-size-4-mobile">
+              A lifelong learner who likes to code
+            </h1>
+            <div className="full">
+              <Link to="/about">
+                <button
+                  className="button is-outlined is-medium mx-5"
+                  data-aos="fade-up"
+                >
+                  About
+                </button>
+              </Link>
+              <Link to="/portfolio">
+                <button
+                  className="button is-outlined is-medium mx-5"
+                  data-aos="fade-down"
+                >
+                  Portfolio
+                </button>
+              </Link>
+              <Link to="/resume">
+                <button
+                  className="button is-outlined is-medium mx-5"
+                  data-aos="fade-up"
+                >
+                  Resume
+                </button>
+              </Link>
+            </div>
+						<div className="is-mobile">
+              <Link to="/about">
+                <button
+                  className="button is-outlined is-normal mx-2"
+                  data-aos="fade-up"
+                >
+                  About
+                </button>
+              </Link>
+              <Link to="/portfolio">
+                <button
+                  className="button is-outlined is-normal mx-2"
+                  data-aos="fade-down"
+                >
+                  Portfolio
+                </button>
+              </Link>
+              <Link to="/resume">
+                <button
+                  className="button is-outlined is-normal mx-2"
+                  data-aos="fade-up"
+                >
+                  Resume
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Hero;
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from "../Hero";
-
+// keeping this component for future additions to the homepage
 
 const Homepage = (props) => {
 
