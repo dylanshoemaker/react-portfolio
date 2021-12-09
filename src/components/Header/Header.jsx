c4634c9d8624f4a300b3dfbc63f10e11136645b2
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-09 10:06:34 -0800

    add bulma burger to header navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,30 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
+  // document.addEventListener('DOMContentLoaded', () => {})
+
+  // // Get all "navbar-burger" elements
+  // const $navbarBurgers = Array.prototype.slice.call(document.getElementsByClassName('navbar-burger'), 0);
+
+  // // Check if there are any navbar burgers
+  // if ($navbarBurgers.length > 0) {
+
+  //   // Add a click event on each of them
+  //   $navbarBurgers.forEach( el => {
+  //     el.addEventListener('onclick', () => {
+
+  //       // Get the target from the "data-target" attribute
+  //       const target = el.dataset.target;
+  //       const $target = document.getElementById(target);
+
+  //       // Toggle the "is-active" className on both the "navbar-burger" and the "navbar-menu"
+  //       el.classList.toggle('is-active');
+  //       $target.classList.toggle('is-active');
+
+  //     });
+  //   });
+  // }
+  function toggleBurgerMenu() {
+    document.querySelector('.navbar-menu').classList.toggle('is-active');
+  }
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-      <Link to="/" className="navbar-item">
-        <strong>Dylan Shoemaker</strong>
-      </Link>
-      </div>
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-          <Link to="/about" className="navbar-item">
-            About
-          </Link>
-          <Link to="/portfolio" className="navbar-item">
-            Portfolio
-          </Link>
-          <Link to="/resume" className="navbar-item">
-            Resume
-          </Link>
+        <Link to="/" className="navbar-item">
+          <strong>Dylan Shoemaker</strong>
+        </Link>
+        <div role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasic"
+          onClick={toggleBurgerMenu}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </div>
       </div>
-    </nav>
+      
+      <div id="navbarBasic" className="navbar-menu">
+        <div className="navbar-end">
+          <Link to="/about" className="navbar-item" onClick={toggleBurgerMenu}>About</Link>
+          <Link to="/portfolio" className="navbar-item" onClick={toggleBurgerMenu}>Portfolio</Link>
+          <Link to="/resume" className="navbar-item" onClick={toggleBurgerMenu}>Resume</Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
