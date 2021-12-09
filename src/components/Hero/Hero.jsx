f2277ae49e78e432ee0abce5b0911b4bc0fd4655
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-08 20:59:12 -0800

    make text more readable

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
 			<section className="hero is-large">
 				<div className="hero-body">
 					<div className="container has-text-centered">
-						<h1 className="subtitle">Hello there, I'm</h1>
+						<h1 className="subtitle profession">Hello there, I'm</h1>
 						<h2 className="title">Dylan</h2>
 						<h1 className="subtitle profession">
 							A lifelong learner who likes to code
