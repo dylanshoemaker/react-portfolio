3ffd08ca75a5edf29e2cb73bc6d600ffdd43d4de
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-16 14:17:01 -0800

    add background to Portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,31 +1,26 @@
-import React from 'react';
-import Project from '../Project';
-
-
+import React from "react";
+import Project from "../Project";
+import "./Portfolio.css"
 const Portfolio = (props) => {
-
-    return (
-        <section className="project">
-
-        <div className="section-dark my-work" id="my-work">
-            <div className="container">
-                <div
-                    className="columns is-multiline"
-                    data-aos="fade-in"
-                    data-aos-easing="linear"
-                >
-                    <div className="column is-12">
-                        <h1 className="title has-text-centered section-title">My Work</h1>
-                    </div>
-
-                    <Project />
-
-                </div>
+  return (
+    <section className="project">
+      <div className="section-dark my-work" id="my-work">
+        <div className="container">
+          <div
+            className="columns is-multiline"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div className="column is-12">
+              <h1 className="title has-text-centered section-title">My Work</h1>
             </div>
-        </div>
 
+            <Project />
+          </div>
+        </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Portfolio;
