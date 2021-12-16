788b2671401d18cc159badefa6f428a93d10cc8e
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-16 14:50:02 -0800

    restructure portfolio component html for box shadow on title, add padding-top to title

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Project from "../Project";
-import "./Portfolio.css"
+import "./Portfolio.css";
 const Portfolio = (props) => {
   return (
     <section className="project">
-      <div className="section-dark my-work" id="my-work">
+      <div className="section-light my-work" id="my-work">
         <div className="container">
+          <div className="column is-12 project">
+            <h1 className="title has-text-centered section-title">My Work</h1>
+          </div>
           <div
             className="columns is-multiline"
             data-aos="fade-in"
             data-aos-easing="linear"
           >
-            <div className="column is-12">
-              <h1 className="title has-text-centered section-title">My Work</h1>
-            </div>
-
             <Project />
           </div>
         </div>
