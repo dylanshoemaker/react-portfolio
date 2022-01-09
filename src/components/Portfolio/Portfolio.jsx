caebede3d80990ecfca1b8232827da26f28426c4
Dylan Shoemaker <me@dylanshoemaker.com>
2022-01-08 17:24:58 -0800

    add margins

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import "./Portfolio.css";
 const Portfolio = () => {
   return (
     <section className="project">
-      <div className="section-light my-work" id="my-work">
+      <div className="section-light" id="my-work">
         <div className="container">
           <div className="column is-12 is-12-mobile project white">
             <h1 className="title is-1 has-text-centered section-title">
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -19,26 +19,32 @@ export default function Resume() {
 
   return (
     <section className="resume">
-      <div className="column is-9 is-12-mobile resume white">
-        <h1 className="title  is-1 has-text-centered section-title">Resume</h1>
-      </div>
-      <div className="container" data-aos="fade-down">
-        <Document
-          className="document"
-          file={file}
-          onLoadSuccess={onDocumentLoadSuccess}
-          options={options}
-        >
-          {Array.from(new Array(numPages), (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-          ))}
-        </Document>
-        <div className="hero-text">
-          <h3 className="button thisIsTheWay">
-            <a href="https://dylanshoemaker.github.io/react-portfolio/resume/dylan_shoemaker_resume.pdf">
-              Download Resume&emsp;<i className="fad fa-download fa-lg"></i>
-            </a>
-          </h3>
+      <div id="resume">
+        <div className="container">
+          <div className="column is-12 is-12-mobile white resume">
+            <h1 className="title  is-1 has-text-centered section-title ">
+              Resume
+            </h1>
+          </div>
+          <div className="container" data-aos="fade-down">
+            <Document
+              className="document"
+              file={file}
+              onLoadSuccess={onDocumentLoadSuccess}
+              options={options}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+            <div className="hero-text">
+              <h3 className="button thisIsTheWay">
+                <a href="https://dylanshoemaker.github.io/react-portfolio/resume/dylan_shoemaker_resume.pdf">
+                  Download Resume&emsp;<i className="fad fa-download fa-lg"></i>
+                </a>
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </section>
